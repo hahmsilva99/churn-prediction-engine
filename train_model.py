69f5c2ad4cd81d7ddb24990650dbff858be50397
Hetti Arachchilage Harshana Madhuwantha Silva <151857717+hahmsilva99@users.noreply.github.com>
2026-05-12 01:48:36 +0530

    finalize the train_model.py and get good model accuracy rate

--- train_model.py
+++ train_model.py
@@ -6,40 +6,40 @@ from imblearn.over_sampling import SMOTE
 import xgboost as xgb
 from sklearn.metrics import accuracy_score
 
-# 1. Data load kirima
-print("Data load wenawa...")
+# 1. Data loading
+print("Data loading now...")
 df = pd.read_excel('churn_data.xlsx')
 
 # 2. Data Cleaning & Preprocessing
-# Total Charges wala his than 0 walin purawanawa (Nama wenas wela thibbe 'Total Charges' kiyala)
+# Filling the blanking spaces in Total Charges using  ' 0 '  
 df['Total Charges'] = pd.to_numeric(df['Total Charges'], errors='coerce').fillna(0)
 
-# Wedak nathi columns saha Target eka leak wena columns ain karanawa
+# Remove unneccessary columns leaking data that columns and Target 
 columns_to_drop = ['CustomerID', 'Count', 'Country', 'State', 'City', 'Zip Code', 'Lat Long', 
                    'Churn Label', 'Churn Score', 'CLTV', 'Churn Reason']
 df.drop(columns=columns_to_drop, inplace=True, errors='ignore')
 
-# Text data numbers walata harawanawa
-print("Data clean karanawa...")
+# Transforming the Text data into numbers 
+print("Data cleaning now ...")
 for column in df.columns:
     if df[column].dtype == 'object':
         le = LabelEncoder()
         df[column] = le.fit_transform(df[column])
 
-# Features (X) saha Target (y - Churn Value eka) wen kirima
+# Seperating Features (X) and Target (y - Churn Value)
 X = df.drop('Churn Value', axis=1)
 y = df['Churn Value']
 
-# Train saha Test widiyata data bedima (80% train, 20% test)
+# Divide the data as Train and Test  data  (80% train, 20% test)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 3. SMOTE walin imbalanced data balance kirima
-print("SMOTE walin data balance karanawa...")
+# 3. Balancing the imbalanced data thruogh SMOTE 
+print("Balancing the imbalanced data thruogh SMOTE...")
 smote = SMOTE(random_state=42)
 X_train_smote, y_train_smote = smote.fit_resample(X_train, y_train)
 
-# 4. XGBoost Model eka Train kirima
-print("XGBoost model eka train wenawa...")
+# 4. Training XGBoost Model
+print("XGBoost model is training now...")
 model = xgb.XGBClassifier(random_state=42, eval_metric='logloss')
 model.fit(X_train_smote, y_train_smote)
 
